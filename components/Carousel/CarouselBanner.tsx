66daa1f523f7541ce55d68fc643196b11d2c5d83
Giang Tran Hoang <hoanggiangg199@gmail.com>
2022-09-23 01:11:06 +0700

    updatE

--- components/Carousel/CarouselBanner.tsx
+++ components/Carousel/CarouselBanner.tsx
@@ -12,19 +12,20 @@ interface CarouselBannerProps {
 const CarouselBanner: FC<CarouselBannerProps> = ({ listItems }) => {
     console.log("@@listItems", listItems)
     return <>
-        <div className='h-[450px] grid grid-cols-5 gap-2 mt-[30px] grid grid-rows-6'>
-            <div className='h-inherit col-span-4 row-span-6'>
+        <div className='h-[450px] grid grid-cols-5 grid-rows-6 gap-4 mt-[30px] '>
+          
+            <div className='h-inherit row-span-5 col-span-4'>
                 <img
-
-                    className='rounded-2xl max-w-full max-h-full object-contain object-top'
+                    className='rounded-2xl'
                     src={"/images/sliderItem/Input.jpeg"} />
             </div>
-
-            <div className="flex flex-col gap-1.5 row-span-1">
-                {listItems.map((item, index) => {
-                    return <>
-                        <div key={index} className={`rouded-xl flex h-[100px] rounded-md border bg-slate-700 p-auto`}>
-                            <div className='h-inherit'>
+            
+            {/* <div className="flex flex-col gap-1.5 h-[100px]"> */}
+            {listItems.map((item, index) => {
+                return <>
+                    <div key={index} className="row-span-1">
+                        <div className='rouded-xl h-[100%] flex rounded-md border bg-slate-700 p-auto'>
+                            <div className='h-[100%]'>
                                 <img
                                     className='rounded max-h-[80%] min-h-[80%] object-contain object-center'
                                     src={"/images/thumbnail/egs-fall-guys-s2-carousel-thumb-315x399-db76b5000c88.jpeg"} />
@@ -33,11 +34,12 @@ const CarouselBanner: FC<CarouselBannerProps> = ({ listItems }) => {
                                 <span className='text-[1vw]'>Fall guys {item}</span>
                             </div>
                         </div>
-                    </>
-                })}
-            </div>
-
+                    </div>
+                </>
+            })}
         </div>
+
+        {/* </div> */}
 
     </>
 }
