3200456e0781be51fdf6257df03221d1cc4d14a3
Giang Tran <hoanggiangg199@gmail.com>
2022-09-23 17:35:53 +0700

    update

--- components/Carousel/CarouselBanner.tsx
+++ components/Carousel/CarouselBanner.tsx
@@ -10,27 +10,28 @@ interface CarouselBannerProps {
 
 
 const CarouselBanner: FC<CarouselBannerProps> = ({ listItems }) => {
+    
     return <>
-        <div className='h-[500px] grid grid-cols-5 grid-rows-6 gap-4 mt-[30px] '>
+        <div className='aspect-[18/9] grid grid-cols-5 grid-rows-6 gap-x-4 gap-y-1.5 mt-[30px] '>
           
             <div className='row-span-6 col-span-4'>
                 <img
-                    className='max-h-full min-h-full rounded-2xl object-contain object-top'
+                    className='max-h-full min-h-full rounded-3xl'
                     src={"/images/sliderItem/Input.jpeg"} />
             </div>
             
             {/* <div className="flex flex-col gap-1.5 h-[100px]"> */}
             {listItems.map((item, index) => {
                 return <>
-                    <div key={index} className="row-span-1 col-span-1">
-                        <div className='rouded-xl h-[100%] flex rounded-md border bg-slate-700 p-auto'>
-                            <div className='h-full'>
+                    <div key={index} className="row-span-1 col-span-1 rounded-2xl border bg-slate-700 flex">
+                        <div className='rouded-xl h-[80%] w-[80%] flex my-auto ml-4'>
+                            <div className='xl:h-[90%] sm:h-[70%] aspect-[15/19] my-auto'>
                                 <img
-                                    className='rounded max-h-[80%] min-h-[80%] object-contain'
+                                    className='rounded-md max-h-full min-h-full'
                                     src={"/images/thumbnail/egs-fall-guys-s2-carousel-thumb-315x399-db76b5000c88.jpeg"} />
                             </div>
-                            <div className='ml-5 grow'>
-                                <span className='text-[1vw]'>Fall guys {item}</span>
+                            <div className='ml-3 m-auto'>
+                                <span className='xl:text-sm lg:text-tiny'>The Rockstart Game Sale {item}</span>
                             </div>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'tiny': '.675rem'
+      },
       width: {
         inherit: 'inherit'
       },
