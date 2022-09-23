4363d8e4c02e4adc99af94e6a5127765d3eb5da9
Giang Tran <hoanggiangg199@gmail.com>
2022-09-23 13:58:26 +0700

    update

--- components/Carousel/CarouselBanner.tsx
+++ components/Carousel/CarouselBanner.tsx
@@ -10,24 +10,23 @@ interface CarouselBannerProps {
 
 
 const CarouselBanner: FC<CarouselBannerProps> = ({ listItems }) => {
-    console.log("@@listItems", listItems)
     return <>
-        <div className='h-[450px] grid grid-cols-5 grid-rows-6 gap-4 mt-[30px] '>
+        <div className='h-[500px] grid grid-cols-5 grid-rows-6 gap-4 mt-[30px] '>
           
-            <div className='h-inherit row-span-5 col-span-4'>
+            <div className='row-span-6 col-span-4'>
                 <img
-                    className='rounded-2xl'
+                    className='max-h-full min-h-full rounded-2xl object-contain object-top'
                     src={"/images/sliderItem/Input.jpeg"} />
             </div>
             
             {/* <div className="flex flex-col gap-1.5 h-[100px]"> */}
             {listItems.map((item, index) => {
                 return <>
-                    <div key={index} className="row-span-1">
+                    <div key={index} className="row-span-1 col-span-1">
                         <div className='rouded-xl h-[100%] flex rounded-md border bg-slate-700 p-auto'>
-                            <div className='h-[100%]'>
+                            <div className='h-full'>
                                 <img
-                                    className='rounded max-h-[80%] min-h-[80%] object-contain object-center'
+                                    className='rounded max-h-[80%] min-h-[80%] object-contain'
                                     src={"/images/thumbnail/egs-fall-guys-s2-carousel-thumb-315x399-db76b5000c88.jpeg"} />
                             </div>
                             <div className='ml-5 grow'>
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,14 +1,12 @@
 import { ReactNode, KeyboardEvent } from 'react'
 import { forwardRef, InputProps as ChakraInputProps, Input as ChakraInput, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
 interface InputProps extends ChakraInputProps {
-    prefixIcon?: ReactNode,
-    suffixIcon?: ReactNode,
     onPressEnter?(value?: string | number): void
 
 }
 
 
-const Input = forwardRef<InputProps, 'input'>(({ prefixIcon, suffixIcon, onPressEnter, ...restProps }, ref: any) => {
+const Input = forwardRef<InputProps, 'input'>(({ onPressEnter, ...restProps }, ref: any) => {
 
     // const inputRef = useRef(null)
     const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -20,18 +18,8 @@ const Input = forwardRef<InputProps, 'input'>(({ prefixIcon, suffixIcon, onPress
 
     // console.log("@@ref",ref?.current?.value)
     return <>
-        <div >
-            <InputGroup>
-                {prefixIcon && <InputLeftElement
-                    pointerEvents='none'
-                    children={prefixIcon}
-                />}
-                <ChakraInput onKeyDown={handleKeyDown} {...restProps} ref={ref} />
-                {suffixIcon && <InputRightElement
-                    pointerEvents='none'
-                    children={suffixIcon}
-                />}
-            </InputGroup>
+        <div>
+            <ChakraInput onKeyDown={handleKeyDown} {...restProps} ref={ref} />
         </div>
 
     </>
